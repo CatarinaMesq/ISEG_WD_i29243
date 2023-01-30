e9293371e4ad2778b20b2a6a3da4e8d12e55544d
CatarinaMesq <anacatarinamvp@gmail.pt>
2023-01-30 17:31:28 +0000

    added Rounting and URL

---
+++ lab_2/app2.py
@@ -0,0 +1,31 @@
+from flask import Flask
+app = Flask (__name__)
+
+@app.route ('/')
+def index ():
+    return 'Hello World!'
+
+@app.route ('/users/<username>')
+def show_user(username):
+    return f'User:{username}'
+
+
+from flask import request
+@app.route('/login', methods =['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        #handle login logic
+    else:
+        # show login form
+        @app.route ('/login', methods =['POST'])
+        def login ():
+            username = request.form ['username']
+            passaword = request.form ['password']
+            #handle login logic
+
+from flask import make_response
+
+@app.route('/')
+def index():
+    response = make_response('Hello world!')
+    response.headers['Content-type'] = 'text/plain'
