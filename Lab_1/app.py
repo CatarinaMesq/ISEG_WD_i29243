13a4fdde16ba008ca994201c42a5972f238a6d02
CatarinaMesq <anacatarinamvp@gmail.pt>
2023-02-03 12:26:46 +0000

    Coments added

--- Lab_1/app.py
+++ Lab_1/app.py
@@ -1,11 +1,13 @@
 #comment
 
 from flask import Flask
-app = Flask (__name__)
+app = Flask(__name__)  #__name__ é a criação de um módulo. Um módulo é um arquivo contendo defenições e instruções de python 
 
 @app.route('/')
 def hello ():
     return "Hello World!"
+
+#quando o módulo é executado por si só como um programa, __name__é defenido para __main__
 
 if __name__ == '__main__':
     app.run()
