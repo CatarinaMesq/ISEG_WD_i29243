ff16d17f1cf52a3ef3f9c34c50dae2b5c4af0933
CatarinaMesq <anacatarinamvp@gmail.pt>
2023-02-05 17:02:35 +0000

    criação da tabela

--- Lab_4/app4.py
+++ Lab_4/app4.py
@@ -1,21 +1,20 @@
-from flask import Flask
-app = Flask(__name__)
+#from flask import Flask
+#app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
-if __name__ == "__main__":
-    app.run()
+#@app.route("/")
+#def hello():
+ #   return "Hello World!"
+#if __name__ == "__main__":
+ #   app.run()
 
 
 import sqlite3
 from sqlite3 import Error
 
 ##Criação de conexão
-
 def caminho_bd ():
     """
-        Criação de um caminho para a base de dados. \n
+        Criação de uma conexão para a base de dados. \n
         O Return é conexção à base de bados Lab_4.
     """
     caminho= "C:\\Users\\CatarinaMesquita\\OneDrive - BUZZSTREET, UNIPESSOAL LDA\Ambiente de Trabalho\\Python\\programas\\base_dados\\Lab_4.db"
@@ -25,3 +24,25 @@ def caminho_bd ():
     except Error as ex:
         print (ex)
     return conexao
+
+vconexao= caminho_bd ()
+
+##Criação de tabela
+
+#vsql = """CREATE TABLE tb_users (
+ #   id_users INTEGER PRIMARY KEY AUTOINCREMENT,
+  #  name TEXT VARCHAR(30))"""
+#def tabela_db(conexao,vsql):
+ #   """
+  #  Criação da tabela "Users" na base de dados Lab_4. \n
+   # Necessário um cursor e um execute.
+    #"""
+    #try:
+     #   c=conexao.cursor ()
+      #  c.execute(vsql)
+       # print("tabela criada")
+    #except Error as ex:
+     #   print (ex)  
+
+#tabela_db(vconexao,vsql)
+#vconexao.close()
