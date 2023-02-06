43cdb5813448187e6f791b8e686e8a4d43a1363e
CatarinaMesq <anacatarinamvp@gmail.pt>
2023-02-06 12:19:46 +0000

    substituição do ficheiro html

---
+++ lab_2/app2.py
@@ -0,0 +1,50 @@
+#importação do módulo Flask do flask enviroment da anaconda e criação de um novo objecto Flask#
+
+from flask import Flask
+from flask import make_response
+from flask import render_template
+from flask import request
+
+app = Flask (__name__)
+
+#criação de uma rota e criação de uma função para processar o pedido
+
+@app.route ('/')
+def index():
+   return 'Hello World!'
+
+#passar variáveis ​​para a rota, use colchetes angulares na rota e inclua uma variável na função:
+
+@app.route ('/users/<username>')
+def show_user(username):
+    return f'User:{username}'      # o "f" serve para "strings literais formatadas", iniciam com a letra "f" e {} para ler a variável 
+
+
+
+@app.route('/login', methods =['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+# handle login logic
+    else:
+    return
+
+        # show login form
+
+
+@app.route('/login', methods = ['POST'] )
+def login():
+    username = request.form['username']
+    password = request.form['password']
+
+@app.route('/')
+def index():
+    response = make_response('Hello World!')
+    response.headers['Content-Type'] = 'text/plain'
+    return response 
+
+@app.route('/')
+def index():
+    name = 'John'
+    return render_template ('index.html', name=name)
+    
+app.run()
