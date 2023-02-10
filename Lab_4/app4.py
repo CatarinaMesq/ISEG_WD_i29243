36dc582f70eda092fa13dd84ed8d8c99662761e3
CatarinaMesq <anacatarinamvp@gmail.pt>
2023-02-10 10:43:03 +0000

    Retirado o simbolo de comentário

--- Lab_4/app4.py
+++ Lab_4/app4.py
@@ -69,51 +69,51 @@ inserir(vconexao,vsql)
 
 ##Apagar dados na tabela (DELETE)
 
-#vsql="""DELETE FROM tb_users where id_users = 1"""
+vsql="""DELETE FROM tb_users where id_users = 1"""
 
-#def apagar(conexao,vsql):
-    #"""Remover dados na tabela "Users" na base de dados Lab_4. \n
-     #   Necessário um cursor, um execute e um commit."""
+def apagar(conexao,vsql):
+    """Remover dados na tabela "Users" na base de dados Lab_4. \n
+       Necessário um cursor, um execute e um commit."""
 
-    #try:
-     #   c= conexao.cursor ()
-      #  c.execute (vsql)
-       # conexao.commit ()
-    #except Error as ex:
-     #   print (ex)
-    #finally:
-     #   print("dados removidos com sucesso")
-#apagar(vconexao,vsql)
+    try:
+        c= conexao.cursor ()
+        c.execute (vsql)
+        conexao.commit ()
+    except Error as ex:
+        print (ex)
+    finally:
+        print("dados removidos com sucesso")
+apagar(vconexao,vsql)
 
 ##UpDate de dados na tabela
 
-#vsql= """UPDATE tb_users SET name = "Bernardo" WHERE id_users = 3"""
+vsql= """UPDATE tb_users SET name = "Bernardo" WHERE id_users = 3"""
 
-#def update(conexao,vsql):
-    #"""Atualização de dados na tabela "Users" na base de dados Lab_4. \n
-     #   Necessário um cursor, um execute e um commit."""
-    #try:
-     #   c= conexao.cursor ()
-      #  c.execute (vsql)
-       # conexao.commit ()
-    #except Error as ex:
-     #   print (ex)
-    #finally:
-     #   print("dados atualizados com sucesso")
-#update(vconexao,vsql)
+def update(conexao,vsql):
+    """Atualização de dados na tabela "Users" na base de dados Lab_4. \n
+        Necessário um cursor, um execute e um commit."""
+    try:
+        c= conexao.cursor ()
+        c.execute (vsql)
+        conexao.commit ()
+    except Error as ex:
+        print (ex)
+    finally:
+        print("dados atualizados com sucesso")
+update(vconexao,vsql)
 
 ##Pesquisa de dados na tabela
 
-#def pesquisa(conexao,vsql):
-    #"""Pesquisa de dados na tabela "Users" na base de dados Lab_4. \n
-     #   Necessário um cursor, um execute. \n
-      #  Vai ser criada uma nova tabela com os dados selecionados. \n
-       # Vamos receber um return: a pesquisa propriamente dita"""
+def pesquisa(conexao,vsql):
+    """Pesquisa de dados na tabela "Users" na base de dados Lab_4. \n
+        Necessário um cursor, um execute. \n
+        Vai ser criada uma nova tabela com os dados selecionados. \n
+        Vamos receber um return: a pesquisa propriamente dita"""
     
-    #c= conexao.cursor ()
-    #c.execute (vsql)
-    #resultado= c.fetchall ()
-    #return resultado 
+    c= conexao.cursor ()
+    c.execute (vsql)
+    resultado= c.fetchall ()
+    return resultado 
 
 vsql= """SELECT * FROM tb_users"""  
 #Para fazer a pesquisa em todas as linhas da tabela, temos de fazer um ciclo for. Para isso vamos transfomar a "pesquisa",
